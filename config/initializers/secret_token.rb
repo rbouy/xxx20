dd81cf5ab5c8d4613746048feaedfdcd357b4a3c
rbouy <rbouy@aerial-group.com>
2012-08-22 23:14:17 +0200

    premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xxx20::Application.config.secret_token = 'dc014ca4f3bc5570873f00bad227eb44ad124afcdd43351ad1904d0af64a5a293121294cf9c2256ba3427670e985fa742db68630ab3e6d9879b584f6d91370f6'
